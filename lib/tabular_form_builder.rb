fea04ad8a80a170e384c89013ce1b39c3ac6458d
Gonçalo Silva <goncalossilva@gmail.com>
2010-05-30 00:03:14 +0100

    More String to SafeBuffer fixes

--- lib/tabular_form_builder.rb
+++ lib/tabular_form_builder.rb
@@ -44,7 +44,7 @@ class TabularFormBuilder < ActionView::Helpers::FormBuilder
       text = options[:label].is_a?(Symbol) ? l(options[:label]) : options[:label]
       text ||= l(("field_" + field.to_s.gsub(/\_id$/, "")).to_sym)
       text += @template.content_tag("span", " *", :class => "required") if options.delete(:required)
-      @template.content_tag("label", text, 
+      @template.content_tag("label", text.html_safe, 
                                      :class => (@object && @object.errors[field] ? "error" : nil), 
                                      :for => (@object_name.to_s + "_" + field.to_s))
   end
