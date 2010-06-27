f8ee93320a0d18736df5f4e6da679587233c217f
Jeremy Kemper <jeremy@bitsweat.net>
2010-06-26 18:21:01 -0700

    Support .bundle/environment

--- config/boot.rb
+++ config/boot.rb
@@ -1,6 +1,7 @@
-require 'rubygems'
-# Set up gems listed in the Gemfile.
-if File.exist?(File.expand_path('../../Gemfile', __FILE__))
+begin
+  require File.expand_path('../../.bundle/environment', __FILE__)
+rescue LoadError
+  require 'rubygems'
   require 'bundler'
   Bundler.setup
 end
