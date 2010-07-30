7df21d667053310161a8d42f93c615a78b3946c3
Gonçalo Silva <goncalossilva@gmail.com>
2010-07-30 01:06:39 +0100

    support for Rails 3 rc1
    changes:
    - fixed boot
    - autoloading lib/ (which is not autoloaded in rc1, but will be on rc2)
    - enabled deprecation warnings in development

--- Gemfile
+++ Gemfile
@@ -1,15 +1,13 @@
 source 'http://rubygems.org'
 
-#gem 'rails', '3.0.0.beta4'
-#gem 'rails', '3.0.0.beta4', :path => '~/rails'
-gem 'rails', '3.0.0.beta4', :git => 'http://github.com/rails/rails.git'
+gem 'rails', '3.0.0.rc', :git => 'http://github.com/rails/rails.git'
 
 gem 'rubytree', '0.7.0'
 
 gem 'ruby-prof', :git => 'http://github.com/wycats/ruby-prof.git'
 
 group :development do
-  gem 'sqlite3-ruby', '~> 1.2.5'
+  gem 'sqlite3-ruby', '~> 1.3.1'
 end
 
 group :test do
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,10 @@
 require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
+# If you have a Gemfile, require the gems listed there, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(:default, Rails.env) if defined?(Bundler)
+
 module Redmine
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -11,7 +15,7 @@ module Redmine
 
     # Add additional load paths for your own custom dirs
     # config.load_paths += %W( #{config.root}/extras )
-    config.autoload_paths += %W( #{Rails.root}/app/sweepers )
+    config.autoload_paths += %W( #{Rails.root}/app/sweepers lib )
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named
