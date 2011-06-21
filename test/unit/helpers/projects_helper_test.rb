56982faeb22832471ad47bbd62a8fab6773d50b9
Toshi MARUYAMA <marutosijp2@gmail.com>
2011-06-21 14:11:16 +0900

    Rails3: fixtures at projects_helper_test.rb
    Fixture::FormatError: Bad data for Configuration::Empty fixture named default (nil)
    
    --HG--
    branch : rails3-straight-20110625-hg-git
    extra : transplant_source : S%A3%B9%3Fz%90%7C%E2%CD%C3%D7%7F%80%FC%C8%2C%C2%8A%93%D0

--- test/unit/helpers/projects_helper_test.rb
+++ test/unit/helpers/projects_helper_test.rb
@@ -20,8 +20,9 @@ require File.expand_path('../../../test_helper', __FILE__)
 class ProjectsHelperTest < ActionView::TestCase
   include ApplicationHelper
   include ProjectsHelper
-  
-  fixtures :all
+
+  # fixtures :all
+  fixtures :projects, :trackers, :issue_statuses, :issues, :enumerations, :users, :issue_categories
 
   def setup
     super
