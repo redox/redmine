ec8472803f14251e534e56c630ab4179c3ad7bdc
Sylvain Utard - redox <sylvain.utard@gmail.com>
2011-09-13 07:06:08 +0200

    Rails3: missing html_safe + missing <em> tags

--- app/helpers/repositories_helper.rb
+++ app/helpers/repositories_helper.rb
@@ -213,12 +213,12 @@ module RepositoriesHelper
                        :size => 60, :required => true,
                        :disabled => (repository && !repository.root_url.blank?)
                          ) +
-                     '<br />' + l(:text_mercurial_repository_note)) +
+                     '<br /><em>'.html_safe + l(:text_mercurial_repository_note)) + '</em>'.html_safe +
     content_tag('p', form.select(
                         :path_encoding, [nil] + Setting::ENCODINGS,
                         :label => l(:field_scm_path_encoding)
                         ) +
-                     '<br />' + l(:text_scm_path_encoding_note))
+                     '<br /><em>'.html_safe + l(:text_scm_path_encoding_note)) + '</em>'.html_safe
   end
 
   def git_field_tags(form, repository)
@@ -227,13 +227,13 @@ module RepositoriesHelper
                        :size => 60, :required => true,
                        :disabled => (repository && !repository.root_url.blank?)
                          ) +
-                      '<br />' + 
-                      l(:text_git_repository_note)) +
+                      '<br /><em>'.html_safe + 
+                      l(:text_git_repository_note)) + '</em>'.html_safe +
     content_tag('p', form.select(
                         :path_encoding, [nil] + Setting::ENCODINGS,
                         :label => l(:field_scm_path_encoding)
                         ) +
-                     '<br />' + l(:text_scm_path_encoding_note)) +
+                     '<br /><em>'.html_safe + l(:text_scm_path_encoding_note)) + '</em>'.html_safe +
     content_tag('p', form.check_box(
                         :extra_report_last_commit,
                         :label => l(:label_git_report_last_commit)
@@ -258,7 +258,7 @@ module RepositoriesHelper
                         :path_encoding, [nil] + Setting::ENCODINGS,
                         :label => l(:field_scm_path_encoding)
                         ) +
-                     '<br />' + l(:text_scm_path_encoding_note))
+                     '<br /><em>'.html_safe + l(:text_scm_path_encoding_note)) + '</em>'.html_safe
   end
 
   def bazaar_field_tags(form, repository)
@@ -280,6 +280,6 @@ module RepositoriesHelper
                         :path_encoding, [nil] + Setting::ENCODINGS,
                         :label => l(:field_scm_path_encoding)
                         ) +
-                     '<br />' + l(:text_scm_path_encoding_note))
+                     '<br /><em>'.html_safe + l(:text_scm_path_encoding_note)) + '</em>'.html_safe
   end
 end
